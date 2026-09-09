5af757bf17d7fdcd5f433d3d64a4eee02d3c1799
bangtrongtu <bangtu2005.com@gmail.com>
2026-09-09 10:17:53 +0700

    done

---
+++ appointment-service/src/test/java/spring_boot/appointmentservice/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ patient-service/src/test/java/spring_boot/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
